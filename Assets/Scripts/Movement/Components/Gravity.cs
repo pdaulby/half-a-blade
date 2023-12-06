7703ef489843d4e7fa2ea8c0f6fd2a088ab4e2da
pdaulby <p.daulby@hotmail.com>
2023-12-06 18:37:13 +0000

    shorter grounded code

--- Assets/Scripts/Movement/Components/Gravity.cs
+++ Assets/Scripts/Movement/Components/Gravity.cs
@@ -11,20 +11,10 @@ namespace Assets.Scripts.Movement.Components
 
         public override void DoUpdate(FixedInput input, PlayerController controller)
         {
+            Debug.Log(controller.IsGrounded);
             if (controller.IsGrounded)
             {
-                //controller.Velocity = new(controller.Velocity.x, 0, controller.Velocity.z);
-                var c1 = Vector3.Cross(controller.GroundedNormal, -controller.Velocity);
-                var tangent = Vector3.Cross(controller.GroundedNormal, c1).normalized;
-                var dot = Vector3.Dot(controller.Velocity.normalized, tangent);
-                Debug.DrawRay(transform.position, c1.normalized, Color.red, 1);
-                Debug.DrawRay(transform.position, tangent*controller.Velocity.magnitude, Color.green, 1);
-                Debug.Log(dot);
-                Vector3 landedDirection = controller.Velocity.magnitude * dot * tangent;
-                controller.Velocity = landedDirection;
-                //ActualVelocity
-                //When grounded, take the Vector3.Project() of the intended velocity against gravity direction, and of actual velocity against gravity direction. 
-                //Then take intended velocity -intendedProjected + actualProjected, and make that the actual velocity
+                controller.Velocity -= Vector3.Dot(controller.Velocity, controller.GroundedNormal) * controller.GroundedNormal;
             }
 
             controller.Velocity += gravity * Time.deltaTime * gravityDirection;
